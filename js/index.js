81c8e1b70a32dfe39b0a8f5b76d15aad9e2b7510
mikaela <59496310+mporazzo@users.noreply.github.com>
2020-07-04 12:11:36 -0700

    animations

---
+++ js/index.js
@@ -0,0 +1,44 @@
+
+$('#content1').mouseenter(function(){
+    $('#content1').animate({
+        marginTop: "-=10px",
+        marginBottom: "+=10px",
+      });
+    }); 
+
+    $('#content1').mouseleave(function(){
+        $('#content1').animate({
+            marginTop: "+=10px",
+            marginBottom: "-=10px",
+          });
+        }); 
+
+    
+      
+        $("#content2").mouseenter(function(){
+            $("#content2").animate({
+                marginTop: "-=10px",
+                marginBottom: "+=10px",
+              });
+            }); 
+
+            $('#content2').mouseleave(function(){
+                $('#content2').animate({
+                    marginTop: "+=10px",
+                    marginBottom: "-=10px",
+                  });
+                }); 
+
+                $("#content3").mouseenter(function(){
+                    $("#content3").animate({
+                        marginTop: "-=10px",
+                        marginBottom: "+=10px",
+                      });
+                    }); 
+        
+                    $('#content3').mouseleave(function(){
+                        $('#content3').animate({
+                            marginTop: "+=10px",
+                            marginBottom: "-=10px",
+                          });
+                        }); 
